fdfb387d24c15d3e86a936d1858b4a37bc0efd9e
Tiago Eusébio <tiago.eusebio@primaverabss.com>
2022-03-24 16:52:58 +0000

    Run FW scripts as Administrator

--- setup_fw.bat
+++ setup_fw.bat
@@ -3,6 +3,11 @@
 ipconfig|find/i "vpn_primavera" || rasdial vpn_primavera
 echo.
 
+@REM Runs the script as Administrator (if not already running as Administrator)
+@REM This is needed in order to change the npmrc
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
 call npmrc elevation
 
 set Arr[0]=services
